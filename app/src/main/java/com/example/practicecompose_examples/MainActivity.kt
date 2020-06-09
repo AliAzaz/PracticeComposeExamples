edfd6e6625a07aa89bbb81ff52b8fc742032ac4d
ali.azaz <ali.azaz.alam@hotmail.com>
2020-06-10 02:52:02 +0500

    Working on CardView style with vertical scroller

--- app/src/main/java/com/example/practicecompose_examples/MainActivity.kt
+++ app/src/main/java/com/example/practicecompose_examples/MainActivity.kt
@@ -7,14 +7,19 @@ import androidx.ui.core.ContentScale
 import androidx.ui.core.Modifier
 import androidx.ui.core.clip
 import androidx.ui.core.setContent
+import androidx.ui.foundation.Icon
 import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
+import androidx.ui.foundation.VerticalScroller
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
-import androidx.ui.layout.Column
-import androidx.ui.layout.fillMaxWidth
-import androidx.ui.layout.padding
-import androidx.ui.layout.preferredHeightIn
+import androidx.ui.layout.*
+import androidx.ui.material.Button
+import androidx.ui.material.Card
+import androidx.ui.material.Divider
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.icons.Icons
+import androidx.ui.material.icons.filled.ArrowForward
+import androidx.ui.material.icons.filled.Check
 import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
@@ -24,27 +29,69 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                ColumnGreeting()
+                CardGreeting()
             }
         }
     }
 }
 
 @Composable
-fun ColumnGreeting() {
-    val image = imageResource(id = R.drawable.scene_01)
-    val imageModifier = Modifier
-        .preferredHeightIn(maxHeight = 160.dp)
-        .fillMaxWidth()
-        .clip(shape = RoundedCornerShape(6.dp))
-    val items = listOf("Ali", "John", "Wick", "Tom")
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Image(image, modifier = imageModifier, contentScale = ContentScale.Crop)
+fun CardGreeting() {
 
-        Text(text = "Hello ${items.random()}!", style = MaterialTheme.typography.h2)
-        Text(text = "Hello ${items.random()}!", style = MaterialTheme.typography.h3)
+    VerticalScroller() {
+        Column {
+            for (x in 0..2) {
+                cardViewImplementer()
+                Divider()
+            }
+        }
+    }
+
+}
+
+@Composable
+fun cardViewImplementer() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .clip(shape = RoundedCornerShape(6.dp))
+    ) {
+
+        Column {
+            Image(
+                imageResource(id = R.drawable.scene_01),
+                modifier = Modifier.preferredHeightIn(160.dp, 260.dp)
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                "A day in Shark Fin Cove",
+                style = MaterialTheme.typography.h4,
+                modifier = Modifier.padding(15.dp, 20.dp, 0.dp, 0.dp)
+            )
+            Column(Modifier.padding(15.dp)) {
+                Text("Davenport, California", style = MaterialTheme.typography.body1)
+                Text("December 2018", style = MaterialTheme.typography.body2)
+                Row(modifier = Modifier.padding(0.dp, 25.dp, 0.dp, 5.dp)) {
+                    Button(
+                        onClick = {},
+                        shape = RoundedCornerShape(5.dp),
+                        modifier = Modifier.padding(5.dp)
+                    ) {
+                        Icon(asset = Icons.Filled.Check)
+                    }
+                    Button(
+                        onClick = {},
+                        shape = RoundedCornerShape(5.dp),
+                        modifier = Modifier.padding(5.dp)
+                    ) {
+                        Icon(asset = Icons.Filled.ArrowForward)
+                    }
+                }
+            }
+        }
+
     }
 }
 
@@ -52,6 +99,6 @@ fun ColumnGreeting() {
 @Composable
 fun DefaultPreview() {
     MaterialTheme {
-        ColumnGreeting()
+        CardGreeting()
     }
 }
