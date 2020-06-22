c3923b1d53f4d5ab1a08b1969db9d630a98f0282
ali.azaz <ali.azaz.alam@hotmail.com>
2020-06-23 01:40:36 +0500

    fixed cardview design template

--- app/src/main/java/com/example/practicecompose_examples/MainActivity.kt
+++ app/src/main/java/com/example/practicecompose_examples/MainActivity.kt
@@ -5,17 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.ContentScale
 import androidx.ui.core.Modifier
-import androidx.ui.core.clip
+import androidx.ui.core.drawShadow
 import androidx.ui.core.setContent
-import androidx.ui.foundation.Icon
-import androidx.ui.foundation.Image
-import androidx.ui.foundation.Text
-import androidx.ui.foundation.VerticalScroller
+import androidx.ui.foundation.*
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.layout.*
 import androidx.ui.material.Button
 import androidx.ui.material.Card
-import androidx.ui.material.Divider
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.icons.Icons
 import androidx.ui.material.icons.filled.ArrowForward
@@ -28,21 +24,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                CardGreeting()
-            }
+            CardGreeting()
         }
     }
 }
 
 @Composable
 fun CardGreeting() {
-
-    VerticalScroller() {
+    VerticalScroller {
         Column {
             for (x in 0..2) {
-                cardViewImplementer()
-                Divider()
+                MaterialTheme {
+                    cardViewImplementer()
+//                    Divider()
+                }
             }
         }
     }
@@ -52,44 +47,51 @@ fun CardGreeting() {
 @Composable
 fun cardViewImplementer() {
     Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(10.dp)
-            .clip(shape = RoundedCornerShape(6.dp))
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier.padding(10.dp)
     ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .drawShadow(5.dp)
+        ) {
 
-        Column {
-            Image(
-                imageResource(id = R.drawable.scene_01),
-                modifier = Modifier.preferredHeightIn(160.dp, 260.dp)
-                    .fillMaxWidth(),
-                contentScale = ContentScale.Crop
-            )
-            Text(
-                "A day in Shark Fin Cove",
-                style = MaterialTheme.typography.h4,
-                modifier = Modifier.padding(15.dp, 20.dp, 0.dp, 0.dp)
-            )
-            Column(Modifier.padding(15.dp)) {
-                Text("Davenport, California", style = MaterialTheme.typography.body1)
-                Text("December 2018", style = MaterialTheme.typography.body2)
-                Row(modifier = Modifier.padding(0.dp, 25.dp, 0.dp, 5.dp)) {
-                    Button(
-                        onClick = {},
-                        shape = RoundedCornerShape(5.dp),
-                        modifier = Modifier.padding(5.dp)
-                    ) {
-                        Icon(asset = Icons.Filled.Check)
-                    }
-                    Button(
-                        onClick = {},
-                        shape = RoundedCornerShape(5.dp),
-                        modifier = Modifier.padding(5.dp)
-                    ) {
-                        Icon(asset = Icons.Filled.ArrowForward)
+            Column {
+                Image(
+                    imageResource(id = R.drawable.scene_01),
+                    modifier = Modifier.preferredHeightIn(160.dp, 260.dp)
+                        .fillMaxWidth(),
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    "A day in Shark Fin Cove",
+                    style = MaterialTheme.typography.h4,
+                    modifier = Modifier.padding(15.dp, 20.dp, 0.dp, 0.dp)
+                )
+                Column(
+                    modifier = Modifier.padding(15.dp)
+                ) {
+                    Text("Davenport, California", style = MaterialTheme.typography.body1)
+                    Text("December 2018", style = MaterialTheme.typography.body2)
+                    Row(modifier = Modifier.padding(0.dp, 25.dp, 0.dp, 5.dp)) {
+                        Button(
+                            onClick = {},
+                            shape = RoundedCornerShape(5.dp),
+                            modifier = Modifier.padding(5.dp)
+                        ) {
+                            Icon(asset = Icons.Filled.Check)
+                        }
+                        Button(
+                            onClick = {},
+                            shape = RoundedCornerShape(5.dp),
+                            modifier = Modifier.padding(5.dp)
+                        ) {
+                            Icon(asset = Icons.Filled.ArrowForward)
+                        }
                     }
                 }
             }
+
         }
 
     }
@@ -98,7 +100,5 @@ fun cardViewImplementer() {
 @Preview
 @Composable
 fun DefaultPreview() {
-    MaterialTheme {
-        CardGreeting()
-    }
+    CardGreeting()
 }
