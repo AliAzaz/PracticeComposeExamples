0a946be0f1d1754e2216bcd1c931424ba87643fd
ali.azaz <ali.azaz.alam@hotmail.com>
2020-07-06 03:26:17 +0500

    working on TODO app

--- app/src/main/java/com/example/practicecompose_examples/MainActivity.kt
+++ app/src/main/java/com/example/practicecompose_examples/MainActivity.kt
@@ -12,13 +12,11 @@ import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.graphics.Color
 import androidx.ui.graphics.drawscope.Stroke
 import androidx.ui.layout.*
-import androidx.ui.layout.ColumnScope.gravity
 import androidx.ui.material.Card
 import androidx.ui.material.MaterialTheme
 import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
-import androidx.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,30 +48,30 @@ fun cardViewImplementer(item: Int) {
             }),
         elevation = 3.dp,
         content = {
-            Box(modifier = Modifier.padding(0.dp, 13.dp, 0.dp, 0.dp),
+            Box(
+                modifier = Modifier.padding(13.dp),
                 children = {
-                    Image(
-                        asset = imageResource(R.drawable.scene_01),
-                        modifier = Modifier.drawBackground(
-                            color = Color.Black,
-                            style = Stroke(4f),
-                            shape = CircleShape
-                        ).preferredSize(120.dp)
-                            .gravity(Alignment.CenterHorizontally)
-                            .clip(CircleShape),
-                        contentScale = ContentScale.Crop
-                    )
-                    Text(
-                        "A day in Shark Fin Cove",
-                        fontSize = 30.sp,
-                        modifier = Modifier.padding(15.dp, 10.dp, 15.dp, 0.dp)
-                    )
-                    Column(
-                        modifier = Modifier.padding(15.dp)
-                    ) {
-                        Text("Davenport, California", style = MaterialTheme.typography.body1)
-                        Text("December 2018", style = MaterialTheme.typography.body2)
-                    }
+                    Row(children = {
+                        Image(
+                            asset = imageResource(R.drawable.scene_01),
+                            modifier = Modifier.drawBackground(
+                                color = Color.Black,
+                                style = Stroke(4f),
+                                shape = CircleShape
+                            ).preferredSize(50.dp)
+                                .clip(CircleShape),
+                            contentScale = ContentScale.Crop
+                        )
+                        Column(
+                            modifier = Modifier.padding(start = 15.dp, end = 15.dp), children = {
+                                Text(
+                                    "Davenport, California",
+                                    style = MaterialTheme.typography.body1
+                                )
+                                Text("December 2018", style = MaterialTheme.typography.body2)
+                            })
+
+                    })
 
                 })
         })
