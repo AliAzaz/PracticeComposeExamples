42a439d25f6278de4e373c19173d9d26bf6b5ce2
ali.azaz <ali.azaz.alam@hotmail.com>
2020-07-05 22:29:33 +0500

    enhanced (ListView --> LazyItems) template

--- app/src/main/java/com/example/practicecompose_examples/MainActivity.kt
+++ app/src/main/java/com/example/practicecompose_examples/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.ui.material.MaterialTheme
 import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
+import androidx.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,12 +31,12 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun CardListViewGreeting() {
-
-    LazyColumnItems(items = (0..5).toList()) { item ->
-        cardViewImplementer(item)
-        Spacer(modifier = Modifier.padding(2.dp))
+    MaterialTheme {
+        LazyColumnItems(items = (0..20).toList()) { item ->
+            cardViewImplementer(item)
+            Spacer(modifier = Modifier.padding(2.dp))
+        }
     }
-
 }
 
 @Composable
@@ -44,41 +45,38 @@ fun cardViewImplementer(item: Int) {
     Card(
         shape = RoundedCornerShape(10.dp),
         modifier = Modifier.padding(10.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .drawShadow(3.dp)
-                .padding(0.dp, 13.dp, 0.dp, 0.dp)
-                .clickable(onClick = {
-                    Toast.makeText(context, "Clicked $item", Toast.LENGTH_SHORT).show()
-                }), children = {
+            .fillMaxWidth().clickable(onClick = {
+                Toast.makeText(context, "Clicked ${item + 1}", Toast.LENGTH_SHORT).show()
+            }),
+        elevation = 3.dp,
+        content = {
+            Box(modifier = Modifier.padding(0.dp, 13.dp, 0.dp, 0.dp),
+                children = {
+                    Image(
+                        asset = imageResource(R.drawable.scene_01),
+                        modifier = Modifier.drawBackground(
+                            color = Color.Black,
+                            style = Stroke(4f),
+                            shape = CircleShape
+                        ).preferredSize(120.dp)
+                            .gravity(Alignment.CenterHorizontally)
+                            .clip(CircleShape),
+                        contentScale = ContentScale.Crop
+                    )
+                    Text(
+                        "A day in Shark Fin Cove",
+                        fontSize = 30.sp,
+                        modifier = Modifier.padding(15.dp, 10.dp, 15.dp, 0.dp)
+                    )
+                    Column(
+                        modifier = Modifier.padding(15.dp)
+                    ) {
+                        Text("Davenport, California", style = MaterialTheme.typography.body1)
+                        Text("December 2018", style = MaterialTheme.typography.body2)
+                    }
 
-                Image(
-                    asset = imageResource(R.drawable.scene_01),
-                    modifier = Modifier.drawBackground(
-                        color = Color.Black,
-                        style = Stroke(4f),
-                        shape = CircleShape
-                    ).preferredSize(120.dp)
-                        .gravity(Alignment.CenterHorizontally)
-                        .clip(CircleShape),
-                    contentScale = ContentScale.FillHeight
-                )
-                Text(
-                    "A day in Shark Fin Cove",
-                    style = MaterialTheme.typography.h4,
-                    modifier = Modifier.padding(15.dp, 10.dp, 0.dp, 0.dp)
-                )
-                Column(
-                    modifier = Modifier.padding(15.dp)
-                ) {
-                    Text("Davenport, California", style = MaterialTheme.typography.body1)
-                    Text("December 2018", style = MaterialTheme.typography.body2)
-                }
-
-            })
-    }
+                })
+        })
 }
 
 @Preview
