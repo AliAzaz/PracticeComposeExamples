f5d5e24b30be9c5fcf0fe3b681c46ece70ee39fc
ali.azaz <ali.azaz.alam@hotmail.com>
2020-08-12 01:55:35 +0500

    *Migrated tutorial to 0.1.0-dev16 *Working on README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+# Image Column Template
+
+![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)
+
+In this tutorial, we'll follow how to extract image resource and populate in **Image** and what is **Column** & **Row**.
+
+
+
+### CODE
+
+
+
+### OUTPUT
+<img alt="Pic-1" src="https://github.com/AliAzaz/PracticeComposeExamples/blob/ImageColumnTemplate/pic_card.png" width="400"/>
+
+
+## CONNECT👍
+Medium: https://medium.com/@ali.azaz.alam
+
+Twitter: https://twitter.com/AliAzazAlam1
+
+Github: https://github.com/aliazaz
+
+LinkedIn: https://www.linkedin.com/in/aliazazalam/
--- app/src/main/java/com/example/practicecompose_examples/MainActivity.kt
+++ app/src/main/java/com/example/practicecompose_examples/MainActivity.kt
@@ -2,23 +2,23 @@ package com.example.practicecompose_examples
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.Composable
-import androidx.ui.core.ContentScale
-import androidx.ui.core.Modifier
-import androidx.ui.core.clip
-import androidx.ui.core.setContent
-import androidx.ui.foundation.Image
-import androidx.ui.foundation.Text
-import androidx.ui.foundation.shape.corner.RoundedCornerShape
-import androidx.ui.layout.Column
-import androidx.ui.layout.fillMaxWidth
-import androidx.ui.layout.padding
-import androidx.ui.layout.preferredHeightIn
-import androidx.ui.material.MaterialTheme
-import androidx.ui.res.imageResource
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.preferredHeightIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.ui.tooling.preview.Preview
-import androidx.ui.unit.dp
-import androidx.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
